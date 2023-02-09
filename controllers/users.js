af835cca9997c7d3e57956ebb38a57a3180547a8
Dmitry Shaposhnikov <ihatepost@gmail.com>
2023-02-09 23:24:36 +0300

    fix cookie samSite atr to 'none'

--- controllers/users.js
+++ controllers/users.js
@@ -47,7 +47,7 @@ const login = (req, res, next) => {
       const token = jwt.sign({ _id: user.id }, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_DEV, { expiresIn: '7d' });
 
       res.cookie('jwt', token, {
-        maxAge: 604800000, httpOnly: true, sameSite: 'None', secure: true,
+        maxAge: 604800000, httpOnly: true, sameSite: 'none', secure: true,
       }).send(user.toJSON());
     })
     .catch(next);
